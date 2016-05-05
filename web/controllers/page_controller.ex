1d99f4b254edcb509a064b85d27a42998c8807ad
r-zzi <greatczzi@gmail.com>
2016-05-05 16:44:36 -0700

    Room url f

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,7 @@ defmodule SketchGuess.PageController do
   use SketchGuess.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    room_id = :erlang.now |> Tuple.to_list |> Enum.map(&Integer.to_string/1) |> Enum.reduce("", &<>/2)
+    render conn, "index.html", room_url: room_id
   end
 end
